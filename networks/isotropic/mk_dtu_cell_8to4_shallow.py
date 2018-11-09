7adb8525887ecc2ce01e02a248fa3fd587cb48e9
Larissa Heinrich <heinrichl@janelia.hhmi.org>
2018-11-09 15:26:05 -0500

    add preliminary label SR scripts

---
+++ networks/isotropic/mk_dtu_cell_8to4_shallow.py
@@ -0,0 +1,129 @@
+from networks import custom_fw_unet as unet
+from networks import ops3d
+import tensorflow as tf
+import json
+
+
+def train_net(labels):
+    input_shape = (196, 196, 196)
+    raw = tf.placeholder(tf.float32, shape=input_shape)
+    raw_bc = tf.reshape(raw, (1, 1,) + input_shape)
+
+    last_fmap8, fov, anisotropy = unet.unet(raw_bc, [12, 12*6, 12*6*6], [48, 12*6, 12*6*6],
+                                            [[2, 2, 2], [2, 2, 2]],
+                                           [[(3, 3, 3), (3, 3, 3)], [(3, 3, 3), (3, 3, 3)],
+                                            [(3, 3, 3), (3, 3, 3)]],
+                                           [[(3, 3, 3), (3, 3, 3)], [(3, 3, 3), (3, 3, 3)],
+                                            [(3, 3, 3), (3, 3, 3)]],
+                                           voxel_size=(8, 8, 8), fov=(8, 8, 8))
+
+    fmap4, anisotropy = ops3d.upsample(last_fmap8, [2, 2, 2], 48, name='labelSR_up', fov=fov, voxel_size=anisotropy)
+    print("final_upsample: " + str(fmap4.shape))
+    last_fmap4, fov = ops3d.conv_pass(fmap4, [(3,3,3), (3,3,3)], num_fmaps=48, fov=fov, voxel_size=anisotropy,
+                                      name='finalconv')
+    print("final convolutions: " + str(last_fmap4.shape))
+    dist_bc, fov = ops3d.conv_pass(
+            last_fmap4,
+            kernel_size=[[1, 1, 1]],
+            num_fmaps=len(labels),
+            activation=None,
+            fov=fov,
+            voxel_size=anisotropy
+            )
+    print("final shape: " + str(dist_bc.shape))
+    output_shape_bc = dist_bc.get_shape().as_list()
+    output_shape_c = output_shape_bc[1:]  # strip the batch dimension
+    output_shape = output_shape_c[1:]
+
+    dist_c = tf.reshape(dist_bc, output_shape_c)
+    network_outputs = tf.unstack(
+        dist_c,len(labels), axis=0)
+
+    gt = []
+    w = []
+    for l in range(len(labels)):
+        gt.append(tf.placeholder(tf.float32, shape=output_shape))
+        w.append(tf.placeholder(tf.float32, shape=output_shape))
+    lb = []
+    lub = []
+    for output_it, gt_it, w_it in zip(network_outputs, gt, w):
+        lb.append(tf.losses.mean_squared_error(gt_it, output_it, w_it))
+        lub.append(tf.losses.mean_squared_error(gt_it, output_it))
+    for label, lb_it, lub_it in zip(labels, lb, lub):
+        tf.summary.scalar('lb_'+label, lb_it)
+        tf.summary.scalar('lub_'+label, lub_it)
+    loss_total = tf.add_n(lb)
+    loss_total_unbalanced = tf.add_n(lub)
+    tf.summary.scalar('loss_total', loss_total)
+    tf.summary.scalar('loss_total_unbalanced', loss_total_unbalanced)
+    opt = tf.train.AdamOptimizer(
+        learning_rate=0.5e-4,
+        beta1=0.95,
+        beta2=0.999,
+        epsilon=1e-8)
+
+    optimizer = opt.minimize(loss_total)
+    merged = tf.summary.merge_all()
+
+    tf.train.export_meta_graph(filename='unet.meta')
+
+    names = {
+        'raw': raw.name,
+        'dist': dist_c.name,
+        'loss_total': loss_total.name,
+        'loss_total_unbalanced': loss_total_unbalanced.name,
+        'optimizer': optimizer.name,
+        'summary': merged.name
+    }
+    for label, output_it, gt_it, w_it, lb_it, lub_it in zip(labels, network_outputs, gt, w, lb, lub):
+        names[label] = output_it.name
+        names['gt_'+label] = gt_it.name
+        names['w_'+label] = w_it.name
+        names['lb_'+label] = lb_it.name
+        names['lub_'+label] = lub_it.name
+
+    with open('net_io_names.json', 'w') as f:
+        json.dump(names, f)
+
+
+def inference_net(labels):
+    input_shape = (340, 340, 340)
+    raw = tf.placeholder(tf.float32, shape=input_shape)
+    raw_bc = tf.reshape(raw, (1, 1,) + input_shape)
+
+    last_fmap8, fov, anisotropy = unet.unet(raw_bc, [12, 12*6, 12*6*6], [48, 12*6, 12*6*6],
+                                            [[2, 2, 2], [2, 2, 2]],
+                                           [[(3, 3, 3), (3, 3, 3)], [(3, 3, 3), (3, 3, 3)],
+                                            [(3, 3, 3), (3, 3, 3)]],
+                                           [[(3, 3, 3), (3, 3, 3)], [(3, 3, 3), (3, 3, 3)],
+                                            [(3, 3, 3), (3, 3, 3)]],
+                                           voxel_size=(8, 8, 8), fov=(8, 8, 8))
+
+    fmap4, anisotropy = ops3d.upsample(last_fmap8, [2, 2, 2], 48, name='labelSR_up', fov=fov, voxel_size=anisotropy)
+    last_fmap4, fov = ops3d.conv_pass(fmap4, [(3, 3, 3), (3, 3, 3)], num_fmaps=48, fov=fov, voxel_size=anisotropy,
+                                      name='finalconv')
+
+    dist_bc, fov = ops3d.conv_pass(
+        last_fmap4,
+        kernel_size=[[1, 1, 1]],
+        num_fmaps=len(labels),
+        activation=None,
+        fov=fov,
+        voxel_size=anisotropy
+    )
+    output_shape_bc = dist_bc.get_shape().as_list()
+    output_shape_c = output_shape_bc[1:]  # strip the batch dimension
+    output_shape = output_shape_c[1:]
+
+    dist_c = tf.reshape(dist_bc, output_shape_c)
+    network_outputs = tf.unstack(
+        dist_c, len(labels), axis=0)
+    tf.train.export_meta_graph(filename='unet_inference.meta')
+
+
+if __name__ == '__main__':
+    labels=['ECS', 'cell', 'plasma_membrane', 'ERES', 'ERES_membrane', 'MVB', 'MVB_membrane', 'er', 'er_membrane',
+            'mito', 'mito_membrane', 'vesicles', 'microtubules']
+    train_net(labels)
+    tf.reset_default_graph()
+    inference_net(labels)
