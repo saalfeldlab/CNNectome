a32f169bc49d0be442ef222fb89721972fbda4fd
Larissa Heinrich <heinrichl@janelia.hhmi.org>
2019-03-05 11:41:03 -0500

    cropping for test blocks and both aligned and unaligned

--- postprocessing/partner_annotations/crop.py
+++ postprocessing/partner_annotations/crop.py
@@ -2,27 +2,44 @@ import z5py
 import os
 import logging
 
-offsets_minicrop = {
-    'A+': (37, 1676, 1598),
-    'B+': (37, 2201, 3294),
-    'C+': (37, 1702, 2135)
-}
-shapes = {
-    'A+': (125, 1529, 1448),
-    'B+': (125, 1701, 2794),
-    'C+': (125, 1424, 1470),
-    'A': (125, 1438, 1322),
-    'B': (125, 1451, 2112),
-    'C': (125, 1578, 1461)
-}
-offsets = {
-     'A+': (37, 1176, 955),
-     'B+': (37, 1076, 1284),
-     'C+': (37, 1002, 1165),
-     'A': (38, 942, 951),
-     'B': (37, 1165, 1446),
-     'C': (37, 1032, 1045)
- }
+#offsets_minicrop = {
+#    'A+': (37, 1676, 1598),
+#    'B+': (37, 2201, 3294),
+#    'C+': (37, 1702, 2135)
+#}
+#shapes = {
+#    'A+': (125, 1529, 1448),
+#    'B+': (125, 1701, 2794),
+#    'C+': (125, 1424, 1470),
+#    'A': (125, 1438, 1322),
+#    'B': (125, 1451, 2112),
+#    'C': (125, 1578, 1461)
+#}
+#offsets = {
+#     'A+': (37, 1176, 955),
+#     'B+': (37, 1076, 1284),
+#     'C+': (37, 1002, 1165),
+#     'A': (38, 942, 951),
+#     'B': (37, 1165, 1446),
+#     'C': (37, 1032, 1045)
+# }
+
+
+offsets = dict()
+offsets['A'] = {True: (38, 942, 951),   False: (38, 911, 911)}
+offsets['B'] = {True: (37, 1165, 1446), False: (37, 911, 911)}
+offsets['C'] = {True: (37, 1032, 1045), False: (37, 911, 911)}
+offsets['A+'] = {True: (37, 1176, 955), False: (37, 911, 911)}
+offsets['B+'] = {True: (37, 1076, 1284), False: (37, 911, 911)}
+offsets['C+'] = {True: (37, 1002, 1165), False: (37, 911, 911)}
+shapes = dict()
+shapes['A'] = {True: (125, 1438, 1322), False: (125, 1250, 1250)}
+shapes['B'] = {True: (125, 1451, 2112), False: (125, 1250, 1250)}
+shapes['C'] = {True: (125, 1578, 1469), False: (125, 1250, 1250)}
+shapes['A+'] = {True: (125, 1556, 1447), False: (125, 1250, 1250)}
+shapes['B+'] = {True: (125, 1701, 2792), False: (125, 1250, 1250)}
+shapes['C+'] = {True: (125, 1424, 1470), False: (125, 1250, 1250)}
+
 
 
 def crop_to_seg(filename_src, dataset_src, filename_tgt, dataset_tgt, offset, shape):
@@ -50,22 +67,24 @@ def crop_to_seg(filename_src, dataset_src, filename_tgt, dataset_tgt, offset, sh
 
 
 def main():
-    samples = ['A+', 'B+', 'C+', 'A', 'B', 'C']
-    filename_src = '/nrs/saalfeld/heinrichl/synapses/pre_and_post/pre_and_post-v3.0/cremi/{0:}.n5'
-    dataset_srcs = ['predictions_it100000/cleft_dist',
-                    'predictions_it100000/pre_dist',
-                    'predictions_it100000/post_dist']
-    filename_tgt = '/nrs/saalfeld/heinrichl/synapses/pre_and_post/pre_and_post-v3.0/cremi/{0:}.n5'
-    dataset_tgts = ['predictions_it100000/cleft_dist_cropped',
-                    'predictions_it100000/pre_dist_cropped',
-                    'predictions_it100000/post_dist_cropped']
-    for sample in samples:
-        logging.info("cropping sample {0:}".format(sample))
-        off = offsets[sample]
-        sh = shapes[sample]
-        for ds_src, ds_tgt in zip(dataset_srcs, dataset_tgts):
-            logging.info("   dataset {0:}".format(ds_src))
-            crop_to_seg(filename_src.format(sample), ds_src, filename_tgt.format(sample), ds_tgt, off, sh)
+    samples = ['A', 'B', 'C']
+    filename_src = '/groups/saalfeld/saalfeldlab/larissa/data/cremieval/{0:}/{1:}.n5'
+    data_eval = ['data2016-aligned', 'data2016-unaligned', 'data2017-aligned', 'data2017-unaligned']
+    dataset_srcs = ['volumes/masks/groundtruth']
+    #filename_tgt = '/nrs/saalfeld/heinrichl/synapses/pre_and_post/pre_and_post-v3.0/cremi/{0:}.n5'
+    dataset_tgts = ['volumes/masks/groundtruth_cropped']
+    for de in data_eval:
+        if 'unaligned' in de:
+            aligned = False
+        else:
+            aligned = True
+        for sample in samples:
+            logging.info("cropping sample {0:}".format(sample))
+            off = offsets[sample][aligned]
+            sh = shapes[sample][aligned]
+            for ds_src, ds_tgt in zip(dataset_srcs, dataset_tgts):
+                logging.info("   dataset {0:}".format(ds_src))
+                crop_to_seg(filename_src.format(de, sample), ds_src, filename_src.format(de, sample), ds_tgt, off, sh)
 
 
 def main_seg():
@@ -86,6 +105,24 @@ def main_seg():
         off = offsets[sample]
         sh = shapes[sample]
         crop_to_seg(filename_src.format(sample), dataset_src, filename_tgt.format(sample), dataset_tgt, off, sh)
+def main_test_blocks():
+    samples = ['A+', 'B+', 'C+']
+    filename_src = '/groups/saalfeld/saalfeldlab/larissa/data/cremieval/{0:}/{1:}.n5'
+    data_eval = ['data2016-aligned', 'data2016-unaligned']
+    datasets_srcs = ['volumes/masks/groundtruth', 'segmentation/multicut']
+    dataset_tgts = ['volumes/masks/groundtruth_cropped', 'volumes/labels/neuron_ids_constis_cropped']
+    for de in data_eval:
+        if 'unaligned' in de:
+            aligned = False
+        else:
+            aligned = True
+        for sample in samples:
+            logging.info("cropping sample {0:}".format(sample))
+            off = offsets[sample][aligned]
+            sh = shapes[sample][aligned]
+            for ds_src, ds_tgt in zip(datasets_srcs, dataset_tgts):
+                logging.info("    dataset {0:}".format(ds_src))
+                crop_to_seg(filename_src.format(de, sample), ds_src, filename_src.format(de, sample), ds_tgt, off, sh)
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
