fca6a00169885282ebfabf1f49aecd461541b44a
Larissa Heinrich <heinrichl@janelia.hhmi.org>
2019-03-05 11:53:23 -0500

    allow hdf or n5 files and define a background label

--- postprocessing/partner_annotations/find_crop.py
+++ postprocessing/partner_annotations/find_crop.py
@@ -2,11 +2,15 @@ import z5py
 import os
 import numpy as np
 from find_partners import bbox_ND
+import h5py
 
 
-def find_crop(filename_src, dataset_src):
-    srcf = z5py.File(filename_src, use_zarr_format=False)
-    bb = bbox_ND(srcf[dataset_src][:])
+def find_crop(filename_src, dataset_src, bg_label=0xfffffffffffffffdL):
+    if filename_src.endswith('.hdf') or filename_src.endswith('.h5'):
+        srcf = h5py.File(filename_src, 'r')
+    else:
+        srcf = z5py.File(filename_src, use_zarr_format=False)
+    bb = bbox_ND(srcf[dataset_src][:]!=bg_label)
     print(srcf[dataset_src].shape)
     off = (bb[0], bb[2], bb[4])
     shape = (bb[1]-bb[0]+1, bb[3]-bb[2]+1, bb[5]-bb[4]+1)
@@ -31,9 +35,18 @@ def find_crop(filename_src, dataset_src):
 
 def main():
     samples = ['A', 'B', 'C']
-    filename_src = '/groups/saalfeld/saalfeldlab/larissa/data/cremi-2017/sample_{' \
-                   '0:}_padded_20170424.aligned.0bg.n5'
-    dataset_src = 'volumes/labels/neuron_ids'
+    filename_src = '/groups/saalfeld/saalfeldlab/projects/cremi-synaptic-partners/sample_{' \
+                   '0:}_padded_20170424.hdf'
+    dataset_src = 'volumes/labels/clefts'
+
+    #samples = ['A+', 'B+', 'C+']
+    #filename_src = '/groups/saalfeld/saalfeldlab/larissa/data/cremieval/data2016-aligned/{0:}.n5'
+    #dataset_src = 'masks/groundtruth'
+    #bg_label = 0
+
+    # filename_src = '/groups/saalfeld/saalfeldlab/larissa/data/cremi-2017/sample_{' \
+    #                '0:}_padded_20170424.aligned.0bg.n5'
+    # dataset_src = 'volumes/labels/neuron_ids'
     offsets = dict()
     shapes = dict()
     for sample in samples:
