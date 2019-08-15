215b21870c581ebf6627d5208c10cd8d6d1c9f40
Larissa Heinrich <heinrichl@janelia.hhmi.org>
2019-08-15 11:41:06 -0400

    mknet script for cleft/pre/post learning now using unet class

--- networks/anisotropic/mk_dtu2_cleftprepost.py
+++ networks/anisotropic/mk_dtu2_cleftprepost.py
@@ -1,139 +1,132 @@
-from networks import unet, ops3d
+from networks import ops3d
+import numpy as np
 import tensorflow as tf
 import json
 
 
-def train_net():
-    input_shape = (43, 430, 430)
-    raw = tf.placeholder(tf.float32, shape=input_shape)
-    raw_bc = tf.reshape(raw, (1, 1,) + input_shape)
+def make_net(unet, added_steps, loss_name='loss_total', mode='train'):
+    # input_shape = (43, 430, 430)
+    names = dict()
+    input_size = unet.min_input_shape
+    input_size_actual = (input_size + added_steps * unet.step_valid_shape).astype(np.int)
+    raw = tf.placeholder(tf.float32, shape=tuple(input_size_actual))
+    names['raw'] = raw.name
+    raw_bc = tf.reshape(raw, (1, 1,) + tuple(input_size_actual))
 
-    last_fmap, fov, anisotropy = unet.unet(raw_bc, 12, 6, [[1, 3, 3], [1, 3, 3], [3, 3, 3]],
-                                           [[(1, 3, 3), (1, 3, 3)], [(1, 3, 3), (1, 3, 3)],
-                                            [(3, 3, 3), (3, 3, 3)], [(3, 3, 3), (3, 3, 3)]],
-                                           [[(1, 3, 3), (1, 3, 3)], [(1, 3, 3), (1, 3, 3)],
-                                            [(3, 3, 3), (3, 3, 3)], [(3, 3, 3), (3, 3, 3)]],
-                                           voxel_size=(10, 1, 1), fov=(10, 1, 1))
+    last_fmap, fov, anisotropy = unet.build(raw_bc)
 
     dist_bc, fov = ops3d.conv_pass(
-            last_fmap,
-            kernel_size=[[1, 1, 1]],
-            num_fmaps=3,
-            activation=None,
-            fov=fov,
-            voxel_size=anisotropy
-            )
+        last_fmap,
+        kernel_size=[[1, 1, 1]],
+        num_fmaps=3,
+        activation=None,
+        fov=fov,
+        voxel_size=anisotropy
+    )
     output_shape_bc = dist_bc.get_shape().as_list()
     output_shape_c = output_shape_bc[1:]
     output_shape = output_shape_c[1:]
+
     dist_c = tf.reshape(dist_bc, shape=output_shape_c)
+    names['dist'] = dist_c.name
     cleft_dist, pre_dist, post_dist = tf.unstack(dist_c, 3, axis=0)
+    names['cleft_dist'] = cleft_dist.name
+    names['pre_dist'] = pre_dist.name
+    names['post_dist'] = post_dist.name
 
-    gt_cleft_dist = tf.placeholder(tf.float32, shape=output_shape)
-    gt_pre_dist = tf.placeholder(tf.float32, shape=output_shape)
-    gt_post_dist = tf.placeholder(tf.float32, shape=output_shape)
+    if mode.lower() == 'train' or mode.lower() == 'training':
+        gt_cleft_dist = tf.placeholder(tf.float32, shape=output_shape)
+        gt_pre_dist = tf.placeholder(tf.float32, shape=output_shape)
+        gt_post_dist = tf.placeholder(tf.float32, shape=output_shape)
 
-    loss_weights_cleft = tf.placeholder(tf.float32, shape=output_shape)
-    loss_weights_pre   = tf.placeholder(tf.float32, shape=output_shape)
-    loss_weights_post  = tf.placeholder(tf.float32, shape=output_shape)
-    mask = tf.placeholder(tf.float32, shape=output_shape)
+        names['gt_cleft_dist'] = gt_cleft_dist.name
+        names['gt_pre_dist'] = gt_pre_dist.name
+        names['gt_post_dist'] = gt_post_dist.name
 
-    loss_balanced_cleft = tf.losses.mean_squared_error(
-        gt_cleft_dist,
-        cleft_dist,
-        loss_weights_cleft
-    )
-    loss_balanced_pre = tf.losses.mean_squared_error(
-        gt_pre_dist,
-        pre_dist,
-        loss_weights_pre
-    )
-    loss_balanced_post = tf.losses.mean_squared_error(
-        gt_post_dist,
-        post_dist,
-        loss_weights_post
-    )
-    loss_unbalanced_cleft = tf.losses.mean_squared_error(gt_cleft_dist, cleft_dist, mask)
-    loss_unbalanced_pre = tf.losses.mean_squared_error(gt_pre_dist, pre_dist, mask)
-    loss_unbalanced_post = tf.losses.mean_squared_error(gt_post_dist, post_dist, mask)
+        loss_weights_cleft = tf.placeholder(tf.float32, shape=output_shape)
+        loss_weights_pre   = tf.placeholder(tf.float32, shape=output_shape)
+        loss_weights_post  = tf.placeholder(tf.float32, shape=output_shape)
 
-    loss_total = loss_balanced_cleft + loss_unbalanced_pre + loss_unbalanced_post
-    tf.summary.scalar('loss_balanced_syn', loss_balanced_cleft)
-    tf.summary.scalar('loss_balanced_pre', loss_balanced_pre)
-    tf.summary.scalar('loss_balanced_post', loss_balanced_post)
+        names['loss_weights_cleft'] = loss_weights_cleft.name
+        names['loss_weights_pre'] = loss_weights_pre.name
+        names['loss_weights_post'] = loss_weights_post.name
 
-    tf.summary.scalar('loss_unbalanced_syn', loss_unbalanced_cleft)
-    tf.summary.scalar('loss_unbalanced_pre', loss_unbalanced_pre)
-    tf.summary.scalar('loss_unbalanced_post', loss_unbalanced_post)
-    tf.summary.scalar('loss_total', loss_total)
+        cleft_mask = tf.placeholder(tf.float32, shape=output_shape)
+        pre_mask = tf.placeholder(tf.float32, shape=output_shape)
+        post_mask = tf.placeholder(tf.float32, shape=output_shape)
 
-    opt = tf.train.AdamOptimizer(
-        learning_rate=0.5e-4,
-        beta1=0.95,
-        beta2=0.999,
-        epsilon=1e-8)
+        names['cleft_mask'] = cleft_mask.name
+        names['pre_mask'] = pre_mask.name
+        names['post_mask'] = post_mask.name
 
-    optimizer = opt.minimize(loss_total)
-    merged = tf.summary.merge_all()
+        loss_balanced_cleft = tf.losses.mean_squared_error(
+            gt_cleft_dist,
+            cleft_dist,
+            loss_weights_cleft * cleft_mask
+        )
+        loss_balanced_pre = tf.losses.mean_squared_error(
+            gt_pre_dist,
+            pre_dist,
+            loss_weights_pre * pre_mask
+        )
+        loss_balanced_post = tf.losses.mean_squared_error(
+            gt_post_dist,
+            post_dist,
+            loss_weights_post * post_mask
+        )
 
-    tf.train.export_meta_graph(filename='unet.meta')
+        names['loss_balanced_cleft'] = loss_balanced_cleft.name
+        names['loss_balanced_pre'] = loss_balanced_pre.name
+        names['loss_balanced_post'] = loss_balanced_post.name
 
-    names = {
-        'raw': raw.name,
-        'cleft_dist': cleft_dist.name,
-        'pre_dist': pre_dist.name,
-        'post_dist': post_dist.name,
-        'gt_cleft_dist': gt_cleft_dist.name,
-        'gt_pre_dist': gt_pre_dist.name,
-        'gt_post_dist': gt_post_dist.name,
-        'loss_balanced_cleft': loss_balanced_cleft.name,
-        'loss_balanced_pre': loss_balanced_pre.name,
-        'loss_balanced_post': loss_balanced_post.name,
-        'loss_unbalanced_cleft': loss_unbalanced_cleft.name,
-        'loss_unbalanced_pre': loss_unbalanced_pre.name,
-        'loss_unbalanced_post': loss_unbalanced_post.name,
-        'loss_total': loss_total.name,
-        'loss_weights_cleft': loss_weights_cleft.name,
-        'loss_weights_pre': loss_weights_pre.name,
-        'loss_weights_post': loss_weights_post.name,
-        'mask': mask.name,
-        'optimizer': optimizer.name,
-        'summary': merged.name}
+        loss_unbalanced_cleft = tf.losses.mean_squared_error(gt_cleft_dist, cleft_dist, cleft_mask)
+        loss_unbalanced_pre = tf.losses.mean_squared_error(gt_pre_dist, pre_dist, pre_mask)
+        loss_unbalanced_post = tf.losses.mean_squared_error(gt_post_dist, post_dist, post_mask)
 
-    with open('net_io_names.json', 'w') as f:
-        json.dump(names, f)
+        names['loss_unbalanced_cleft'] = loss_unbalanced_cleft.name
+        names['loss_unbalanced_pre'] = loss_unbalanced_pre.name
+        names['loss_unbalanced_post'] = loss_unbalanced_post.name
 
+        loss_total = loss_balanced_cleft + loss_balanced_pre + loss_balanced_post
+        loss_total_unbalanced = loss_unbalanced_cleft + loss_unbalanced_pre + loss_unbalanced_post
+        names['loss_total'] = loss_total.name
+        names['loss_total_unbalanced'] = loss_total_unbalanced.name
 
-def inference_net():
-    input_shape = (91, 862, 862)
-    raw = tf.placeholder(tf.float32, shape=input_shape)
-    raw_bc = tf.reshape(raw, (1, 1,) + input_shape)
+        tf.summary.scalar('loss_balanced_cleft', loss_balanced_cleft)
+        tf.summary.scalar('loss_balanced_pre', loss_balanced_pre)
+        tf.summary.scalar('loss_balanced_post', loss_balanced_post)
 
-    last_fmap, fov, anisotropy = unet.unet(raw_bc, 12, 6, [[1, 3, 3], [1, 3, 3], [3, 3, 3]],
-                                           [[(1, 3, 3), (1, 3, 3)], [(1, 3, 3), (1, 3, 3)],
-                                            [(3, 3, 3), (3, 3, 3)], [(3, 3, 3), (3, 3, 3)]],
-                                           [[(1, 3, 3), (1, 3, 3)], [(1, 3, 3), (1, 3, 3)],
-                                            [(3, 3, 3), (3, 3, 3)], [(3, 3, 3), (3, 3, 3)]],
-                                           voxel_size=(10, 1, 1), fov=(10, 1, 1))
+        tf.summary.scalar('loss_unbalanced_cleft', loss_unbalanced_cleft)
+        tf.summary.scalar('loss_unbalanced_pre', loss_unbalanced_pre)
+        tf.summary.scalar('loss_unbalanced_post', loss_unbalanced_post)
+        tf.summary.scalar('loss_total', loss_total)
+        tf.summary.scalar('loss_total_unbalanced', loss_total_unbalanced)
 
-    dist_bc, fov = ops3d.conv_pass(
-            last_fmap,
-            kernel_size=[[1, 1, 1]],
-            num_fmaps=3,
-            activation=None,
-            fov=fov,
-            voxel_size=anisotropy
-            )
-    output_shape_bc = dist_bc.get_shape().as_list()
-    output_shape_c = output_shape_bc[1:]
-    #output_shape = output_shape_c[1:]
-    dist_c = tf.reshape(dist_bc, shape=output_shape_c)
-    cleft_dist, pre_dist, post_dist = tf.unstack(dist_c, 3, axis=0)
+        opt = tf.train.AdamOptimizer(
+            learning_rate=0.5e-4,
+            beta1=0.95,
+            beta2=0.999,
+            epsilon=1e-8)
+        if loss_name == 'loss_total':
+            optimizer = opt.minimize(loss_total)
+        elif loss_name == 'loss_total_unbalanced':
+            optimizer = opt.minimize(loss_total_unbalanced)
+        else:
+            raise ValueError(loss_name + " not defined")
+        names['optimizer'] = optimizer.name
 
-    tf.train.export_meta_graph(filename='unet_inference.meta')
+        merged = tf.summary.merge_all()
+        names['summary'] = merged.name
 
 
-if __name__ == '__main__':
-    train_net()
-    tf.reset_default_graph()
-    inference_net()
+
+        with open('net_io_names.json', 'w') as f:
+            json.dump(names, f)
+    elif mode.lower() == 'inference' or mode.lower() == 'prediction' or mode.lower() == 'pred':
+        pass
+    else:
+        raise ValueError("unknown mode for netowrk construction: {0:}".format(mode))
+    net_name = 'unet_' + mode
+    tf.train.export_meta_graph(filename=net_name+'.meta')
+    return net_name, input_size_actual, output_shape
+
