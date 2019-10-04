e9ae4f4bccb71aed71dd3c71a82d6487228691d6
Larissa Heinrich <heinrichl@janelia.hhmi.org>
2019-10-03 22:14:03 -0400

    update default version of docker start script to new version

--- docker_start_scripts/run_train.sh
+++ docker_start_scripts/run_train.sh
@@ -2,6 +2,8 @@
 WD=$(pwd)
 NAME=$(basename $(pwd))
 NAME=$(basename $(dirname $(pwd)))-$NAME-training
+RUNSCRIPT=${@:1:$#-1}
+GPU="device=${*: -1}"
 USER_ID=${UID}
 docker rm -f $NAME
 #rm snapshots/*
@@ -11,17 +13,19 @@ cd /groups/saalfeld
 cd /nrs/saalfeld
 cd $WD
 #neptunes5thmoon/gunpowder:v0.3-pre6-dask1 \
+echo $RUNSCRIPT
 docker run \
-    --gpus 1 \
+    --gpus $GPU  \
     --rm \
     -u ${USER_ID} \
     -v /groups/turaga:/groups/turaga:rshared \
     -v /groups/saalfeld:/groups/saalfeld:rshared \
     -v /groups/cosem/cosem:/groups/cosem/cosem:rshared \
+    -v /nrs/cosem:/nrs/cosem:rshared \
     -v /nrs/saalfeld:/nrs/saalfeld:rshared \
     -w ${PWD} \
     -ti \
     --name ${NAME} \
-    neptunes5thmoon/cnnectome:v.2.0.dev1 \
+    neptunes5thmoon/cnnectome:v2.0.dev2 \
     /bin/bash -c "export OMP_NUM_THREADS=1; nvidia-smi;
-    mprof run -T 1 -CM python -u $1 2>&1 | tee -a logfile"
+    mprof run -T 1 -CM python -u $RUNSCRIPT 2>&1 | tee -a logfile"
