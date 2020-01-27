a851793f8575c18ef83b99bff86dcef433039637
Larissa Heinrich <heinrichl@janelia.hhmi.org>
2020-01-27 18:22:13 -0500

    generalize scripts for synapse training

---
+++ CNNectome/training/anisotropic/train_dist_syn.py
@@ -0,0 +1,372 @@
+from gunpowder import *
+from gunpowder.tensorflow import *
+from gunpowder.contrib import (
+    ZeroOutConstSections,
+    AddDistance,
+    AddPrePostCleftDistance,
+    TanhSaturate,
+)
+import fuse
+import tensorflow as tf
+import os
+import math
+import json
+import csv
+import logging
+import numpy as np
+import time
+
+
+def make_cleft_to_prepostsyn_neuron_id_dict(csv_files, filter_comments):
+    cleft_to_pre = dict()
+    cleft_to_post = dict()
+    for csv_f in csv_files:
+        logging.info("Reading csv file {0:}".format(csv_f))
+        f = open(csv_f, "r")
+        reader = csv.DictReader(
+            f,
+            fieldnames=[
+                "pre_label",
+                "pre_id",
+                "pre_x",
+                "pre_y",
+                "pre_z",
+                "pre_comment",
+                "post_label",
+                "post_id",
+                "post_x",
+                "post_y",
+                "post_z",
+                "post_comment",
+                "cleft",
+            ],
+        )
+        next(reader)
+        for row in reader:
+            if int(row["cleft"]) > 0:
+                if (
+                    row["pre_comment"] not in filter_comments
+                    and row["post_comment"] not in filter_comments
+                ):
+                    if int(row["pre_label"]) > 0:
+                        try:
+                            cleft_to_pre[int(row["cleft"])].add(int(row["pre_label"]))
+                        except KeyError:
+                            cleft_to_pre[int(row["cleft"])] = {int(row["pre_label"])}
+                    if int(row["post_label"]) > 0:
+                        try:
+                            cleft_to_post[int(row["cleft"])].add(int(row["post_label"]))
+                        except KeyError:
+                            cleft_to_post[int(row["cleft"])] = {int(row["post_label"])}
+    return cleft_to_pre, cleft_to_post
+
+
+def train_until(
+    max_iteration,
+    cremi_dir,
+    samples,
+    n5_filename_format,
+    csv_filename_format,
+    filter_comments,
+    labels,
+    net_name,
+    input_shape,
+    output_shape,
+    loss_name,
+    aug_mode,
+    dt_scaling_factor=50,
+    cache_size=5,
+    num_workers=10,
+    min_masked_voxels=17561.0,
+    voxel_size=Coordinate((40,4,4))
+):
+    def label_filter(cond_f):
+        return [ll for ll in labels if cond_f(ll)]
+
+    def get_label(name):
+        filter = label_filter(lambda l: l.labelname == name)
+        if len(filter) > 0:
+            return filter[0]
+        else:
+            return None
+
+    def network_setup():
+        # load net_io_names.json
+        with open("net_io_names.json", "r") as f:
+            net_io_names = json.load(f)
+
+        # find checkpoint from previous training, start a new one if not found
+        if tf.train.latest_checkpoint("."):
+            start_iteration = int(tf.train.latest_checkpoint(".").split("_")[-1])
+            if start_iteration >= max_iteration:
+                logging.info("Network has already been trained for {0:} iterations".format(start_iteration))
+            else:
+                logging.info("Resuming training from {0:}".format(start_iteration))
+        else:
+            start_iteration = 0
+            logging.info("Starting fresh training")
+
+        # define network inputs
+        inputs = dict()
+        inputs[net_io_names["raw"]] = ak_raw
+        inputs[net_io_names["mask"]] = ak_training
+        for label in labels:
+            inputs[net_io_names["mask_" + label.labelname]] = label.mask_key
+            inputs[net_io_names["gt_dist_" + label.labelname]] = label.gt_dist_key
+            if label.scale_loss or label.scale_key is not None:
+                inputs[net_io_names["w_" + label.labelname]] = label.scale_key
+
+        # define network outputs
+        outputs = dict()
+        for label in labels:
+            outputs[net_io_names[label.labelname]] = label.pred_dist_key
+        return net_io_names, start_iteration, inputs, outputs
+
+    keep_thr = float(min_masked_voxels) / np.prod(output_shape)
+    max_distance = 2.76 * dt_scaling_factor
+
+    ak_raw = ArrayKey("RAW")
+    ak_alpha = ArrayKey("ALPHA_MASK")
+    ak_neurons = ArrayKey("GT_NEURONS")
+    ak_training = ArrayKey("TRAINING_MASK")
+    ak_integral = ArrayKey("INTEGRAL_MASK")
+    ak_gradient = ArrayKey("LOSS_GRADIENT")
+    ak_clefts = ArrayKey("GT_CLEFTS")
+
+    input_size = Coordinate(input_shape) * voxel_size
+    output_size = Coordinate(output_shape) * voxel_size
+    net_io_names, start_iteration, inputs, outputs = network_setup()
+
+    # specifiy which Arrays should be requested for each batch
+    request = BatchRequest()
+    request.add(ak_raw, input_size, voxel_size=voxel_size)
+    request.add(ak_neurons, output_size, voxel_size=voxel_size)
+    request.add(ak_clefts, output_size, voxel_size=voxel_size)
+    request.add(ak_training, output_size, voxel_size=voxel_size)
+    request.add(ak_integral, output_size, voxel_size=voxel_size)
+    for l in labels:
+        request.add(l.mask_key, output_size, voxel_size=voxel_size)
+        request.add(l.scale_key, output_size, voxel_size=voxel_size)
+        request.add(l.gt_dist_key, output_size, voxel_size=voxel_size)
+
+    # specify specs for output
+    array_specs_pred = dict()
+    for l in labels:
+        array_specs_pred[l.pred_dist_key] = ArraySpec(voxel_size=voxel_size, interpolatable=True)
+
+    snapshot_data = {
+        ak_raw: "volumes/raw",
+        ak_training: "volumes/masks/training",
+        ak_clefts: "volumes/masks/gt_clefts",
+        ak_gradient: "volumes/loss_gradient"
+    }
+
+    # specify snapshot data layout
+    for l in labels:
+        snapshot_data[l.mask_key] = "volumes/masks/" + l.labelname
+        snapshot_data[l.pred_dist_key] = "volumes/labels/pred_dist_" + l.labelname
+        snapshot_data[l.gt_dist_key] = "volumes/labels/gt_dist_" + l.labelname
+
+    # specify snapshot request
+    snapshot_request_dict = {ak_gradient: request[ak_clefts]}
+    for l in labels:
+        snapshot_request_dict[l.pred_dist_key] = request[l.gt_dist_key]
+    snapshot_request = BatchRequest(snapshot_request_dict)
+
+    csv_files = [
+        os.path.join(cremi_dir, csv_filename_format.format(sample))
+        for sample in samples
+    ]
+
+    cleft_to_pre, cleft_to_post = make_cleft_to_prepostsyn_neuron_id_dict(
+        csv_files, filter_comments
+    )
+
+    data_providers = []
+
+    for sample in samples:
+        logging.info("Adding sample {0:}".format(sample))
+        datasets = {
+            ak_raw: "volumes/raw",
+            ak_training: "volumes/masks/training",
+            ak_integral: "volumes/masks/groundtruth_integral",
+            ak_clefts: "volumes/labels/clefts",
+            ak_neurons: "volumes/labels/neuron_ids"
+        }
+        specs = {
+            ak_clefts: ArraySpec(interpolatable=False),
+            ak_training: ArraySpec(interpolatable=False),
+            ak_integral: ArraySpec(interpolatable=False)
+        }
+        for l in labels:
+            datasets[l.mask_key] = "volumes/masks/groundtruth"
+            specs[l.mask_key] = ArraySpec(interpolatable=False)
+
+        n5_source = ZarrSource(
+            os.path.join(cremi_dir, n5_filename_format.format(sample)),
+            datasets=datasets,
+            array_specs=specs,
+        )
+        data_providers.append(n5_source)
+
+    data_sources = tuple(
+        provider +
+        Normalize(ak_raw) +  # ensures RAW is in float in [0, 1]
+        RandomLocationWithIntegralMask(
+            integral_mask=ak_integral, min_masked=keep_thr
+        )
+        + Reject(ak_training, min_masked=0.999)
+        + Reject(ak_clefts, min_masked=0.0, reject_probability=0.95)
+        for provider in data_providers
+    )
+
+    artifact_source = (
+        Hdf5Source(
+            os.path.join(cremi_dir, "sample_ABC_padded_20160501.defects.hdf"),
+            datasets={
+                ArrayKeys.RAW: "defect_sections/raw",
+                ArrayKeys.ALPHA_MASK: "defect_sections/mask",
+            },
+            array_specs={
+                ArrayKeys.RAW: ArraySpec(voxel_size=(40, 4, 4)),
+                ArrayKeys.ALPHA_MASK: ArraySpec(voxel_size=(40, 4, 4)),
+            },
+        )
+        + RandomLocation(min_masked=0.05, mask=ak_alpha)
+        + Normalize(ak_raw)
+        +
+        # IntensityAugment(ArrayKeys.RAW, 0.9, 1.1, -0.1, 0.1, z_section_wise=True) +
+        ElasticAugment((4, 40, 40), (0, 2, 2), (0, math.pi / 2.0), subsample=8)
+        + SimpleAugment(transpose_only=[1, 2], mirror_only=[1, 2])
+    )
+
+    train_pipeline = data_sources + RandomProvider()
+    if aug_mode == "deluxe":
+        slip_ignore = [ak_clefts, ak_training, ak_neurons, ak_integral]
+        for l in labels:
+            slip_ignore += l.mask_key
+        train_pipeline = (
+            train_pipeline
+            + fuse.ElasticAugment(
+                (40, 4, 4),
+                (4, 40, 40),
+                (0.0, 2.0, 2.0),
+                (0, math.pi / 2.0),
+                spatial_dims=3,
+                subsample=8,
+        )
+            + SimpleAugment(transpose_only=[1, 2], mirror_only=[1, 2])
+            + fuse.Misalign(
+                40,
+                prob_slip=0.05,
+                prob_shift=0.05,
+                max_misalign=(10, 10),
+                ignore_keys_for_slip=tuple(slip_ignore),
+        )
+            + IntensityAugment(ak_raw, 0.9, 1.1, -0.1, 0.1, z_section_wise=True)
+            + DefectAugment(
+                ak_raw,
+                prob_missing=0.03,
+                prob_low_contrast=0.01,
+                prob_artifact=0.03,
+                artifact_source=artifact_source,
+                artifacts=ak_raw,
+                artifacts_mask=ak_alpha,
+                contrast_scale=0.5,
+        )
+        )
+    elif aug_mode == "classic":
+        train_pipeline = (
+            train_pipeline
+            + fuse.ElasticAugment(
+                (40, 4, 4),
+                (4, 40, 40),
+                (0.0, 0.0, 0.0),
+                (0, math.pi / 2.0),
+                spatial_dims=3,
+                subsample=8,
+        )
+            + fuse.SimpleAugment(transpose_only=[1, 2], mirror_only=[1, 2])
+            + IntensityAugment(ak_raw, 0.9, 1.1, -0.1, 0.1, z_section_wise=True)
+            + DefectAugment(
+                ak_raw,
+                prob_missing=0.03,
+                prob_low_contrast=0.01,
+                prob_artifact=0.03,
+                artifact_source=artifact_source,
+                artifacts=ak_raw,
+                artifacts_mask=ak_alpha,
+                contrast_scale=0.5,
+        )
+        )
+    elif aug_mode == "lite":
+        train_pipeline = (
+            train_pipeline
+            + fuse.ElasticAugment(
+                (40, 4, 4),
+                (4, 40, 40),
+                (0.0, 0.0, 0.0),
+                (0, math.pi / 2.0),
+                spatial_dims=3,
+                subsample=8,
+        )
+            + fuse.SimpleAugment(transpose_only=[1, 2], mirror_only=[1, 2])
+            + IntensityAugment(ak_raw, 0.9, 1.1, -0.1, 0.1, z_section_wise=False)
+        )
+    else:
+        pass
+    train_pipeline = (
+        train_pipeline
+        + IntensityScaleShift(ak_raw, 2, -1)
+        + ZeroOutConstSections(ak_raw))
+    clefts = get_label("clefts")
+    if clefts is not None:
+        train_pipeline += AddDistance(
+            label_array_key=clefts.gt_key,
+            distance_array_key=clefts.gt_dist_key,
+            mask_array_key=clefts.mask_key,
+            max_distance=2.76 * dt_scaling_factor,
+        )
+    pre = get_label("pre")
+    post = get_label("post")
+    if pre is not None or post is not None:
+        train_pipeline += AddPrePostCleftDistance(
+            ak_clefts,
+            ak_neurons,
+            pre.gt_dist_key if pre is not None else None,
+            post.gt_dist_key if post is not None else None,
+            pre.mask_key if pre is not None else None,
+            post.mask_key if post is not None else None,
+            cleft_to_pre,
+            cleft_to_post,
+            include_cleft=False,
+            max_distance=2.76 * dt_scaling_factor,
+        )
+    for l in labels:
+        train_pipeline += TanhSaturate(l.gt_dist_key, dt_scaling_factor)
+    for l in labels:
+        train_pipeline += BalanceByThreshold(labels=l.gt_dist_key, scales=l.scale_key,
+                                             mask=(l.mask_key, ak_training),
+                                             threshold=l.thr)
+
+    train_pipeline = (train_pipeline
+                      + PreCache(cache_size=cache_size, num_workers=num_workers)
+                      + Train(net_name, optimizer=net_io_names["optimizer"], loss=net_io_names[loss_name],
+                              inputs=inputs, summary=net_io_names["summary"], log_dir="log", save_every=500,
+                              log_every=5, outputs=outputs,
+                              gradients={net_io_names["cleft_dist"]: ArrayKeys.LOSS_GRADIENT},
+                              )
+                      + Snapshot(snapshot_data, every=500, output_filename="batch_{iteration}.hdf",
+                                 output_dir="snapshots/", additional_request=snapshot_request
+                                 )
+                      + PrintProfilingStats(every=50)
+                      )
+
+    logging.info("Starting training...")
+    with build(train_pipeline) as pp:
+        for i in range(start_iteration, max_iteration+1):
+            start_it = time.time()
+            pp.request_batch(request)
+            time_it = time.time() - start_it
+            logging.info("it{0:}: {1:}".format(i+1, time_it))
+    logging.info("Training finished")
---
+++ CNNectome/training/synapse_template.py
@@ -0,0 +1,213 @@
+import logging
+from CNNectome.utils.label import *
+from CNNectome.networks.mk_dist_unet_with_labels import make_net
+from CNNectome.networks import unet_class
+from CNNectome.training.anisotropic.train_dist_syn import train_until
+from CNNectome.validation.single_block_inference import single_block_inference
+from gunpowder import Coordinate
+import json
+import numpy as np
+import argparse
+
+logging.basicConfig(level=logging.INFO)
+
+# running parameters
+max_iteration = 500000
+cache_size = 5
+num_workers = 10
+
+# voxel size parameters
+voxel_size = Coordinate((40,4,4))
+
+# network parameters
+steps_train = 4
+steps_inference = 11
+padding = "valid"
+constant_upsample = True
+trans_equivariant = True
+feature_widths_down = [12, 12 * 6, 12 * 6 ** 2, 12 * 6 ** 3]
+feature_widths_up = [12 , 12 * 6, 12 * 6 ** 2, 12 * 6 ** 3]
+downsampling_factors = [(1, 3, 3), (1, 3, 3) , (3, 3, 3)]
+kernel_sizes_down = [
+    [(1, 3, 3), (1, 3, 3)],
+    [(1, 3, 3), (1, 3, 3)],
+    [(3, 3, 3), (3, 3, 3)],
+    [(3, 3, 3), (3, 3, 3)]
+]
+kernel_sizes_up = [
+    [(1, 3, 3), (1, 3, 3)],
+    [(1, 3, 3), (1, 3, 3)],
+    [(3, 3, 3), (3, 3, 3) ]
+]
+
+# additional network parameters for upsampling network
+#upsample_factor = tuple(voxel_size_input/voxel_size)
+#final_kernel_size = [(3,) * 3, (3,) * 3]
+#final_feature_width = 12 * 6
+
+# groundtruth source parameters
+cremi_dir = "/groups/saalfeld/saalfeldlab/projects/fafb-synapses/cremi2019/"
+samples = ["A", "B", "C"]
+n5_filename_format = "sample_{0:}.n5"
+csv_filename_format = "sample_{0:}_clefts_to_seg.csv"
+filter_comments = []
+
+# training parameters
+loss_name="loss_total"
+aug_mode="deluxe"
+
+# groundtruth construction parameters
+min_masked_voxels = 17561.
+dt_scaling_factor = 50
+labels = list()
+labels.append(Label("clefts", None, thr=0., scale_loss=True, separate_labelset=True))
+labels.append(Label("pre", None, thr=-0.5, scale_loss=True, separate_labelset=True))
+labels.append(Label("post", None, thr=-0.5, scale_loss=True, separate_labelset=True))
+
+
+def build_net(steps=steps_inference, mode="inference"):
+    unet = unet_class.UNet(
+        feature_widths_down,
+        feature_widths_up,
+        downsampling_factors,
+        kernel_sizes_down,
+        kernel_sizes_up,
+        padding=padding,
+        constant_upsample=constant_upsample,
+        trans_equivariant=trans_equivariant,
+        input_voxel_size=voxel_size,
+        input_fov=voxel_size
+    )
+    net, input_shape, output_shape = make_net(unet, labels, steps, loss_name=loss_name, mode=mode)
+    logging.info(
+        "Built {0:} with input shape {1:} and output_shape {2:}".format(
+            net, input_shape, output_shape
+        )
+    )
+
+    return net, input_shape, output_shape
+
+
+def test_memory_consumption(steps=steps_train, mode="train"):
+    from CNNectome.utils.test_memory_consumption import Test
+
+    net, input_shape, output_shape = build_net(steps, mode=mode)
+    with open("net_io_names.json", "r") as f:
+        net_io_names = json.load(f)
+    input_arrays = dict()
+    requested_outputs = dict()
+    input_arrays[net_io_names["raw"]] = np.random.random(
+        input_shape.astype(np.int)
+    ).astype(np.float32)
+    for l in labels:
+        if mode.lower() == "train" or mode.lower() == "training":
+            input_arrays[net_io_names["mask"]] = np.random.randint(0, 1, output_shape).astype(
+                np.float32)
+            input_arrays[net_io_names["gt_" + l.labelname]] = np.random.random(
+                output_shape
+            ).astype(np.float32)
+            if l.scale_loss or l.scale_key is not None:
+                input_arrays[net_io_names["w_" + l.labelname]] = np.random.random(
+                    output_shape
+                ).astype(np.float32)
+            input_arrays[net_io_names["mask_" + l.labelname]] = np.random.random(
+                output_shape
+            ).astype(np.float32)
+        requested_outputs[l.labelname] = net_io_names[l.labelname]
+
+    t = Test(
+        net,
+        requested_outputs,
+        net_io_names["optimizer"],
+        net_io_names["loss_total"],
+        mode=mode,
+    )
+    t.setup()
+    for it in range(100):
+        t.train_step(input_arrays, iteration=it + 1)
+
+
+def train(steps=steps_train):
+    net_name, input_shape, output_shape = build_net(steps=steps, mode="train")
+    train_until(
+        max_iteration,
+        cremi_dir,
+        samples,
+        n5_filename_format,
+        csv_filename_format,
+        filter_comments,
+        labels,
+        net_name,
+        input_shape,
+        output_shape,
+        loss_name,
+        aug_mode,
+        dt_scaling_factor=dt_scaling_factor,
+        cache_size=cache_size,
+        num_workers=num_workers,
+        min_masked_voxels=min_masked_voxels,
+        voxel_size=voxel_size
+    )
+
+
+def inference(steps=steps_inference):
+    net_name, input_shape, output_shape = build_net(steps=steps, mode="inference")
+    outputs = [l.labelname for l in labels]
+    single_block_inference(net_name, input_shape, output_shape, ckpt, outputs, input_file, coordinate=coordinate,
+                           output_file=output_file, voxel_size_input=voxel_size_input, voxel_size_output=voxel_size)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser("Build, train or test memory consumption for a U-Net")
+    parser.add_argument("script", type=str, help="Pick script that should be run",
+                        choices=["train", "build", "test_mem", "inference"], default="train")
+    parser.add_argument("--mode", type=str, help="for build and test_mem specify whether to run for inference or "
+                                               "training network", choices=["training", "inference"],
+                        )
+    parser.add_argument("--db_username", type=str, help="username for the database")
+    parser.add_argument("--db_password", type=str, help="password for the database")
+    parser.add_argument("--ckpt", type=str, help="checkpoint file to use for inference")
+    parser.add_argument("--input_file", type=str, help="n5 file for input data to predict from")
+    parser.add_argument("--output_file", type=str, help="n5 file to write inference output to", default="prediction.n5")
+    parser.add_argument("--coordinate", type=int, help="upper left coordinate of block to predict from (input)",
+                        default=(0, 0, 0), nargs='+')
+    args = parser.parse_args()
+    mode = args.mode
+    db_username = args.db_username
+    db_password = args.db_password
+    ckpt = args.ckpt
+    input_file = args.input_file
+    output_file = args.output_file
+    coordinate = tuple(args.coordinate)
+
+    if args.script == "train":
+        if mode == "inference":
+            raise ValueError("script train should not be run with mode inference")
+        else:
+            mode = "training"
+        assert db_username is not None and db_password is not None, \
+            "db_username and db_password need to be given to run training"
+
+    elif args.script == "inference":
+        if mode == "training":
+            raise ValueError("script inference should not be run with mode training")
+        else:
+            mode = "inference"
+        assert ckpt is not None and input_file is not None, \
+            "ckpt and input_file need to be given to run inference"
+
+    if mode == "inference":
+        steps = steps_inference
+    elif mode == "training":
+        steps = steps_train
+    else:
+        raise ValueError("mode needs to be given to run script {0:}".format(args.script))
+
+    if args.script == "train":
+        train(steps)
+    elif args.script == "build":
+        build_net(steps, mode)
+    elif args.script == "test_mem":
+        test_memory_consumption(steps, mode)
+    elif args.script == "inference":
+        inference(steps)
