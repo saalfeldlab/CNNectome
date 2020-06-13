f81e6ba8f846f1effef12b42ea12fe56f0b4039f
Larissa Heinrich <heinrichl@janelia.hhmi.org>
2020-06-13 00:55:09 -0400

    script to rewrite csv backend with db entries

---
+++ CNNectome/validation/organelles/transfer_db_to_csv.py
@@ -0,0 +1,32 @@
+import argparse
+from CNNectome.utils.hierarchy import hierarchy
+from CNNectome.utils.cosem_db import MongoCosemDB, CosemCSV
+
+db_host = "cosem.int.janelia.org:27017"
+gt_version = "v0003"
+training_version = "v0003.2"
+eval_results_csv_folder = "/groups/cosem/cosem/computational_evaluation/{training_version:}/csv/".format(training_version=training_version)
+
+
+def transfer(db_username, db_password):
+    db = MongoCosemDB(db_username, db_password, host=db_host, gt_version=gt_version, training_version=training_version)
+    eval_col = db.access("evaluation", training_version)
+    csv_d = CosemCSV(eval_results_csv_folder)
+    for l in hierarchy.keys():
+        csv_d.erase(l)
+    for db_entry in eval_col.find():
+        csv_d.write_evaluation_result(db_entry["path"], db_entry["dataset"], db_entry["setup"], db_entry["iteration"],
+                                      db_entry["label"], db_entry["crop"], db_entry["threshold"], db_entry["metric"],
+                                      db_entry["metric_params"], db_entry["value"])
+
+
+def main():
+    parser = argparse.ArgumentParser("")
+    parser.add_argument("--db_username", type=str, help="username for the database")
+    parser.add_argument("--db_password", type=str, help="password for the database")
+    args = parser.parse_args()
+    transfer(args.db_username, args.db_password)
+
+
+if __name__ == "__main__":
+    main()
