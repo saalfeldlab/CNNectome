81ab3986af76fde091b4069c240220a0eb458d4b
Larissa Heinrich <heinrichl@janelia.hhmi.org>
2020-07-07 12:24:51 -0400

    zarr has been released with necessary changes

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ REQUIRED = [
     "scipy",
     "cython",
     "h5py",
-    "zarr @ git+https://github.com/zarr-developers/zarr@master",
+    "zarr>=2.4.0",
     "joblib",
     "scikit-image",
     "matplotlib",
@@ -43,7 +43,6 @@ DEPENDENCY_LINKS = [
     "git+https://github.com/neptunes5thmoon/gunpowder.git@dist_transform_py3#egg=gunpowder",
     "git+https://github.com/neptunes5thmoon/fuse.git@my_pipinstallable_version#egg=fuse",
     "git+https://github.com/neptunes5thmoon/malis.git@fix_setup#egg=malis",
-    "git+https://github.com/zarr-developers/zarr.git@master#egg=zarr",
     "git+https://github.com/neptunes5thmoon/simpleference.git@master#egg=simpleference[zarr]"
 ]
 
