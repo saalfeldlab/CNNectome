319dda3068392fd88f0034f4a88ccc05a43fac5c
Larissa <heinrichl@janelia.hhmi.org>
2021-02-12 16:48:25 -0500

    imporve job names in inference script

--- shell_scripts/submit_inference.sh
+++ shell_scripts/submit_inference.sh
@@ -1,22 +1,22 @@
 #!/usr/bin/env bash
-WD=$(pwd)
-WDTL=$(basename $(pwd))
+SETUPDIR=${PWD#${PWD%/*/*/*}/}
+SETUP=${SETUPDIR////_}
 DP=$(basename $3)
 for i in {0..15}; do
     while : ; do
-            running_jobs=`bjobs -noheader -o "job_name" | grep "$3-$4-$WDTL" | wc -l`
-	    echo $3-$4-$WDTL
+            running_jobs=`bjobs -noheader -o "job_name" | grep "$3-$4-$SETUP" | wc -l`
+	    echo $3-$4-$SETUP
             echo "waiting for $running_jobs jobs to finish"
             [[ $running_jobs -gt 0 ]] || break
-            sleep 360
+            sleep 120
     done
-    complete=`python check_inference_complete.py ${@}`;
+    complete=`python3 check_inference_complete.py ${@}`;
     echo $complete;
     if [  $complete -eq 1 ]; then
        echo ending
        exit 0
     else
 echo "Submitting job"
-       bsub -J "$3-$4-$WDTL-prep" -P cosem -n 2 -gpu "num=1" -q gpu_short  -o "$DP-$4-$WDTL-output.log" -e "$DP-$4-$WDTL-error.log" -W 100 ./run_inference_cluster.sh ${@}
+       bsub -J "$3-$4-$SETUP-prep" -P cosem -n 2 -gpu "num=1" -q gpu_m6000  -o "$DP-$4-$SETUP-output.log" -e "$DP-$4-$SETUP-error.log" -We 100 ./run_inference_cluster.sh ${@}
     fi
 done
