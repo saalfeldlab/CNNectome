65ea8b97fbfc45c7db6c3b679261d1c03647936d
Larissa <heinrichl@janelia.hhmi.org>
2021-02-17 15:00:03 -0500

    improve shell scripts

--- shell_scripts/run_inference_cluster.sh
+++ shell_scripts/run_inference_cluster.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 WD=$(pwd)
-WDTL=$(basename $(pwd))
+SETUPDIR=${PWD#${PWD%/*/*/*}/}
+SETUP=${SETUPDIR////_}
 DP=$(basename $3)
 RUNSCRIPT=${@}
 echo $RUNSCRIPT
@@ -12,18 +13,19 @@ singularity exec \
             -B /scratch/$USER:/tmp,/groups/saalfeld,/nrs/saalfeld,/groups/turaga,/groups/cosem/cosem,/nrs/cosem \
             --pwd $WD \
             /groups/saalfeld/home/heinrichl/singularity-builds/cnnectome.sif \
-            /bin/bash --norc -c "export OMP_NUM_THREADS=1; umask 0002; python -u unet_inference_template.py prepare $RUNSCRIPT"
+            /bin/bash --norc -c "export OMP_NUM_THREADS=1; umask 0002; python -u inference_config.py prepare $RUNSCRIPT"
 echo "Running inference split over $1 jobs"
 
 for i in $(seq 0 `expr $1 - 1`);
 do
-    bsub -J "$3-$4-$WDTL-$i" -P cosem -n 2 -gpu "num=1" -q gpu_short -o "$DP-$4-$WDTL-$i-output.log" \
-    -e "$DP-$4-$WDTL-$i-error.log" -W 240 \
+    bsub -J "$3-$4-$SETUP-$i" -P cosem -n 2 -gpu "num=1" -q gpu_short -o "$DP-$4-$SETUP-$i-output.log" \
+    -e "$DP-$4-$SETUP-$i-error.log" -W 240 \
     singularity exec \
                 --nv \
                 --containall \
                 -B /scratch/$USER:/tmp,/groups/saalfeld,/nrs/saalfeld,/groups/turaga,/groups/cosem/cosem,/nrs/cosem \
                 --pwd $WD \
                 /groups/saalfeld/home/heinrichl/singularity-builds/cnnectome.sif \
-                /bin/bash --norc -c "export OMP_NUM_THREADS=1; umask 0002; python -u unet_inference_template.py inference $i ${*:2} ";
+                /bin/bash --norc -c "export OMP_NUM_THREADS=1; umask 0002; python -u inference_config.py inference $i
+                ${*:2} ";
 done;
