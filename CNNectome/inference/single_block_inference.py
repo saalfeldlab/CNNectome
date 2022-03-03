bde8528ed5adc0a4aefca3b19ecc4c2144f2cbcc
Larissa Heinrich <heinrichl@janelia.hhmi.org>
2022-03-03 15:52:34 -0500

    fix: :bug: use standardized name for network io file
    the scripts for network builts use net_io_names.json as the default name for the io json

--- CNNectome/inference/single_block_inference.py
+++ CNNectome/inference/single_block_inference.py
@@ -51,7 +51,7 @@ def single_block_inference(net_name, input_shape, output_shape, ckpt, outputs, i
 
 
     # prepare input and output definition for model
-    with open('{0:}_io_names.json'.format(net_name))as f:
+    with open('net_io_names.json'.format(net_name))as f:
         net_io_names = json.load(f)
     network_input_key = net_io_names[input]
     network_output_keys = []
