811b2b5a982336a8f064949013ef3314f6d695a7
Larissa Heinrich <heinrichl@janelia.hhmi.org>
2022-07-14 15:28:25 -0400

    test: :white_check_mark: add test for evaluation metrics: symmetry and perfect score
    Symmetry tests for certain pairs of metrics that result matches when reversing what's truth and what's test. Perfect score tests whether the result for a given metric is "perfect" when test==truth

--- tests/validation/organelles/test_segmentation_metrics.py
+++ tests/validation/organelles/test_segmentation_metrics.py
@@ -1,7 +1,7 @@
-import pytest
 import numpy as np
+import pytest
 import skimage.draw
-
+from CNNectome.validation.organelles import segmentation_metrics
 from CNNectome.validation.organelles.segmentation_metrics import *
 
 
@@ -76,11 +76,142 @@ class TestEvaluator:
 
         score_unmasked = evaluator_unmasked.compute_score(metric)
         score_masked = evaluator_masked.compute_score(metric)
-        print(score_unmasked)
+
         if np.isnan(score_unmasked):
             assert np.isnan(score_masked)
         else:
             assert score_unmasked == score_masked
+
+    @pytest.mark.parametrize(
+        "metric_1to2,metric_2to1",
+        [
+            ("dice", "dice"),
+            ("f1_score_with_tolerance", "f1_score_with_tolerance"),
+            ("f1_score", "f1_score"),
+            ("false_discovery_rate", "false_negative_rate"),
+            ("hausdorff", "hausdorff"),
+            ("jaccard", "jaccard"),
+            ("mean_false_bdy_distance", "mean_false_bdy_distance"),
+            ("mean_false_distance_clipped", "mean_false_distance_clipped"),
+            ("mean_false_negative_distance", "mean_false_positive_distance"),
+            ("mean_false_negative_bdy_distance", "mean_false_positive_bdy_distance"),
+            ("mean_false_distance", "mean_false_distance"),
+            (
+                "mean_false_negative_distance_clipped",
+                "mean_false_positive_distance_clipped",
+            ),
+            ("recall_with_tolerance", "precision_with_tolerance"),
+            ("voi", "voi"),
+            ("recall", "precision"),
+        ],
+    )
+    def test_symmetry(self, metric_1to2, metric_2to1):
+        arr1_binary_masked, _ = skimage.draw.random_shapes(
+            (100, 100),
+            7,
+            1,
+            allow_overlap=True,
+            multichannel=False,
+            num_channels=1,
+            min_size=7,
+            shape="ellipse",
+        )
+        arr1_binary_masked = arr1_binary_masked != 255
+        arr1_binary_masked = np.stack(
+            [
+                arr1_binary_masked,
+            ]
+            * 100
+        )
+        arr2_binary_masked, _ = skimage.draw.random_shapes(
+            (100, 100),
+            7,
+            1,
+            allow_overlap=True,
+            multichannel=False,
+            num_channels=1,
+            min_size=7,
+            shape="ellipse",
+        )
+        arr2_binary_masked = arr2_binary_masked != 255
+        arr2_binary_masked = np.stack(
+            [
+                arr2_binary_masked,
+            ]
+            * 100
+        )
+        mask = np.zeros((100, 100, 100))
+
+        mid_slice = (slice(10, -10, None),) * 3
+
+        mask[mid_slice] = np.ones(mask[mid_slice].shape)
+
+        metric_params = {"clip_distance": 10, "tol_distance": 10}
+        evaluator_1to2 = Evaluator(
+            arr1_binary_masked,
+            arr2_binary_masked,
+            False,
+            False,
+            metric_params,
+            resolution=(1, 1, 1),
+            mask=mask,
+        )
+        evaluator_2to1 = Evaluator(
+            arr2_binary_masked,
+            arr1_binary_masked,
+            False,
+            False,
+            metric_params,
+            resolution=(1, 1, 1),
+            mask=mask,
+        )
+
+        score_1to2 = evaluator_1to2.compute_score(metric_1to2)
+        score_2to1 = evaluator_2to1.compute_score(metric_2to1)
+
+        if np.isnan(score_1to2):
+            assert np.isnan(score_2to1)
+        else:
+            assert score_1to2 == score_2to1
+
+    @pytest.mark.parametrize("metric", EvaluationMetrics)
+    def test_perfect_score(self, metric):
+        truth_binary, _ = skimage.draw.random_shapes(
+            (100, 100),
+            7,
+            1,
+            allow_overlap=True,
+            multichannel=False,
+            num_channels=1,
+            min_size=7,
+            shape="ellipse",
+        )
+        truth_binary = truth_binary != 255
+        truth_binary = np.stack(
+            [
+                truth_binary,
+            ]
+            * 100
+        )
+        test_binary = truth_binary.copy()
+        metric_params = {"clip_distance": 20, "tol_distance": 30}
+        evaluator = Evaluator(
+            truth_binary,
+            test_binary,
+            False,
+            False,
+            metric_params,
+            resolution=(1, 1, 1),
+            mask=None,
+        )
+        limit_idx = 0 if segmentation_metrics.sorting(metric) == 1 else 1
+        print(limit_idx)
+        limits = segmentation_metrics.limits(metric)
+        print(limits)
+        perfect_score = limits[limit_idx]
+        print(perfect_score)
+        # perfect_score = segmentation_metrics.best(metric)(segmentation_metrics.limits(metric))
+        assert evaluator.compute_score(metric) == perfect_score
 
     @pytest.mark.parametrize("metric", EvaluationMetrics)
     def test_mask_none(self, metric):
